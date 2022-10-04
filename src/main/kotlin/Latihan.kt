eda0923198bfa120cdcc81de8289dc4fe323e455
nabilazahras <nabilazahrasalsabila@gmail.com>
2022-10-04 16:25:54 +0700

    Progress_Challange_1

---
+++ src/main/kotlin/Latihan.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var hargaParkir = Float
+    var jamParkir = Int
+
+    jamParkir = 26
+
+    val hargaAwal = 1
+    val hargaSelanjutnya = 0.5
+    val hargaMaksimal = 15
+
+    if (jamParkir <= 5) {
+        hargaParkir = (jamParkir * hargaAwal).toFloat()
+    }else if ((jamParkir> 5) && (jamParkir <24 )) {
+            hargaParkir= (5 + ((jamParkir-5)*hargaSelanjutnya)).toFloat()
+        } else if{
+        (jamParkir = 24) {
+            hargaParkir = (hargaMaksimal).toFloat()
+        } else
+        hargaParkir= (15 + ((jamParkir-24)*hargaSelanjutnya)).toFloat()
+    println("Jam Parkir: $jamParkir ")
+    println("Harga Parkir: $hargaParkir ")
+}
