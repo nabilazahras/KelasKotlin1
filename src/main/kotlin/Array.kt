c62fa1f25f6818dfdc04d87f44e2ae1b4771be0a
nabilazahras <nabilazahrasalsabila@gmail.com>
2022-10-06 08:41:40 +0700

    Contoh

---
+++ src/main/kotlin/Array.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val cars = arrayOf("Honda", "Toyota", "BMW")
+
+    cars.forEach {car ->
+        println("Ini adalah mobil $car")
+    }
+
+    val honda = cars.filter {car -> car == "Honda"}
+    println(honda)
+
+    var nilaiMahasiswa = arrayOf(30, 70, 85, 64, 66)
+    val lulusMatkulProgramming = nilaiMahasiswa.filter {
+            nilai -> nilai >= 65
+    }
+
+    println(lulusMatkulProgramming)
+}
